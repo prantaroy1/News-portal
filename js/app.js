3690b8cd68bd962d8a9599a0b086a0dd31398f80
prantaroy <prantaroy186@gmail.com>
2022-09-03 20:41:36 +0800

    Co-authored-by: Ibrahim Khalil <rahatibrahim@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Loading Categories data from https://openapi.programming-hero.com/api/news/categories
+
 let loadCategories = async () => {
 	let url = `https://openapi.programming-hero.com/api/news/categories`;
 	try {
@@ -9,7 +11,7 @@ let loadCategories = async () => {
 	}
 };
 
-//  Displaying Categories Name---------//
+//  Displaying Categories Name in the web page
 
 let displayCategories = async () => {
 	let categories = await loadCategories();
@@ -77,21 +79,21 @@ let displayCategoriesNews = async (news, name) => {
 		newsDiv.innerHTML = `
 	        <div class="card flex sm:flex-row p-4 bg-white shadow-xl my-5 hover:shadow-sky-600 hover:duration-150 mb-10 ">
 	            <figure><img src="${
-							newsItem.thumbnail_url
+								newsItem.thumbnail_url
 							}" alt="Album" style="width: 100%; height: 100%;"></figure>
 	            <div class="card-body">
 	            <h2 class="card-title text-black font-bold text-2xl">${newsItem.title}</h2>
 	            <p>${
-					newsItem.details.length > 20
-					? newsItem.details.slice(0, 200) + '...'
-					: newsItem.details
-					}
+								newsItem.details.length > 20
+									? newsItem.details.slice(0, 200) + '...'
+									: newsItem.details
+							}
 	                </p>
 	            <p>${
-					newsItem.details.length > 20
-					? newsItem.details.slice(201, 400) + '...'
-					: newsItem.details
-					}
+								newsItem.details.length > 20
+									? newsItem.details.slice(201, 400) + '...'
+									: newsItem.details
+							}
 	                </p>
 
 	                <div class="grid grid-cols-1 sm:grid-cols-3 items-center text-center mt-3">
@@ -102,12 +104,12 @@ let displayCategoriesNews = async (news, name) => {
 	                        </div>
 	                        <div>
 	                        <h1>${
-								newsItem.author.name === null
-								? 'Author Not found'
-								: newsItem.author.name === ''
-							    ? 'Author Not found'
-							    : newsItem.author.name
-					            }
+														newsItem.author.name === null
+															? 'Author Not found'
+															: newsItem.author.name === ''
+															? 'Author Not found'
+															: newsItem.author.name
+													}
                             </h1>
 	                        </div>
 	                    </div>
@@ -209,7 +211,11 @@ let blogInfo = () => {
       Difference Between var, let and const
     </div>
     <div class="collapse-content mx-3 text-2xl  bg-white text-black font-semibold"> 
-      <p>var declarations are globally scoped or function scoped while let and const are block scoped. var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared.
+      <p>Variables declared with var are in the function scope.
+      Variables declared as let are in the block scope.
+      Variables declared as const are in the block scope.
+      While variables declared with var are hoisted to the enclosing scope, variables declared with let or const are not initialized until their definition is evaluated.
+      At the top level, variables declared with var, unlike ones declared with let or const, create a property on the global object.
       </p>
     </div>
   </div>
@@ -219,7 +225,7 @@ let blogInfo = () => {
     Difference between arrow function and normal function
     </div>
     <div class="collapse-content mx-3 text-2xl  bg-white text-black font-semibold"> 
-      <p>Since regular functions are constructible, they can be called using the new keyword. However, the arrow functions are only callable and not constructible, i.e arrow functions can never be used as constructor functions. Hence, they can never be invoked with the new keyword.Both regular JavaScript functions and arrow functions work in a similar manner but there are some differences between them.The first and most obvious difference between arrow functions and regular functions is their syntax. Not only do they look different, but arrow functions also provide an implicit return shorthand and allow parenthesis around a single argument to be omitted.Inside a regular function, execution context (i.e. the value of this) is dynamic. This means that the value of this depends on how the function was invoked.On the other hand, an arrow function does not define its own execution context. This results in an arrow function's this being resolved lexically.
+      <p>Both regular JavaScript functions and arrow functions work in a similar manner but there are some differences between them.The first and most obvious difference between arrow functions and regular functions is their syntax. Not only do they look different, but arrow functions also provide an implicit return shorthand and allow parenthesis around a single argument to be omitted.Inside a regular function, execution context (i.e. the value of this) is dynamic. This means that the value of this depends on how the function was invoked.On the other hand, an arrow function does not define its own execution context. This results in an arrow function's this being resolved lexically.
       </p>
     </div>
   </div>
@@ -229,26 +235,26 @@ let blogInfo = () => {
     Difference between map filter find and foreach
     </div>
     <div class="collapse-content mx-3 text-2xl  bg-white text-black font-semibold"> 
-      <p>The main difference between this two methods is that forEach allows you to change elements of original array and returns undefined and map does not allow you to change original array and intended for making new array based on original one.
+      <p>
       The main difference between forEach and filter is that forEach just loop over the array and executes the callback but filter executes the callback and check its return value. map() loop through the elements allocates memory and stores return values by iterating main array.find: Return the first element which satisfies the condition.
       </p>
     </div>
   </div>
-  <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box text-center ">
-  <div class="collapse-title text-3xl font-bold border-yellow-300 border-4 p-4">
-  Why should we use template string ?
+
+    <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box text-center ">
+    <div class="collapse-title text-3xl font-bold border-yellow-300 border-4 p-4">
+    Why should we use template string
+    </div>
+    <div class="collapse-content mx-3 text-2xl  bg-white text-black font-semibold"> 
+      <p>
+      Template strings are a powerful feature of modern JavaScript released in ES6. It lets us insert/interpolate variables and expressions into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements.
+      </p>
+    </div>
   </div>
-  <div class="collapse-content mx-3 text-2xl  bg-white text-black font-semibold"> 
-    <p> Template strings are a powerful feature of modern JavaScript released in ES6. It lets us insert/interpolate variables and expressions into strings without needing to concatenate like in older versions of JavaScript. It allows us to create strings that are complex and contain dynamic elements.
-    Template literals provide an easy way to interpolate variables and expressions into strings. The method is called string interpolation.
-    </p>
-  </div>
-</div>
+ 
 
 
-
-  </div>
-  `;
+    </div>
+    `;
 };
-
 blogInfo();
